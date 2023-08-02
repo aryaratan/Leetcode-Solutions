e94c9adfd4d6a5453022612c174f3ca64e8a4c57
Pragyaratan <85423364+aryaratan@users.noreply.github.com>
2023-08-02 08:27:34 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -7,7 +7,7 @@ class Solution
 {
 	public:
 	//Function to return list containing vertices in Topological order.
-	
+	// top sort using DFS
 	void dfs(int c, vector<int> adj[], bool vis[], stack<int> &st){
 	    vis[c] = true;
 	    
@@ -22,9 +22,55 @@ class Solution
 	    st.push(c);
 	}
 	
+	vector<int> bsf(int V, vector<int> adj[]){
+	    vector<int> ans;
+	    queue<int> q;
+	    vector<int> inDeg(V,0);
+	    unordered_map<int, int> mp;
+	    for(int i=0;i<V;i++){
+	        for(int j=0;j<adj[i].size();j++){
+	            mp[adj[i][j]]++;
+	        }
+	    }
+	    
+	   for(int i=0;i<V;i++){
+	       inDeg[i] = mp[i];
+	       if(mp[i] == 0){
+	           q.push(i);
+	       }
+	   }
+	    while(!q.empty()){
+	        int t = q.front();
+	        ans.push_back(t);
+	        q.pop();
+	        for(auto x:adj[t]){
+	            inDeg[x]--;
+	            if(inDeg[x] == 0){
+	                q.push(x);
+	            }
+	        }
+	    }
+	   // for(auto x:ans){
+	   //     cout << x << " ";
+	   // }
+	    return ans;
+	}
+	
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
 	    // code here
+
+// an ordering of nodes(vertices) such that for every pair (u,v) having an edge u will appeare before v in that ordering.
+	    
+	    
+	    // bsf Code
+	    
+	    return bsf(V,adj);
+	    
+	    
+	    
+	    // dfs code
+	    /*
 	    vector<int> ans;
 	    stack<int> st;
 	    bool vis[V] = {false};
@@ -39,6 +85,7 @@ class Solution
 	        st.pop();
 	    }
 	    return ans;
+	    */
 	}
 };
 

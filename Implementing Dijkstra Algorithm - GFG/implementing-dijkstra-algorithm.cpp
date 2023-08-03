70eff67ca0997caf638bff9fc28cb5d6edfb786d
Pragyaratan <85423364+aryaratan@users.noreply.github.com>
2023-08-03 12:00:17 +0530

    Added solution - LeetHub

--- Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
@@ -11,29 +11,55 @@ class Solution
     vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
     {
         // Code here
+        
+        
+        // used for finding shortest cost of path (weighted edges)
+        
         vector<int> ans(V, INT_MAX);
         ans[S] = 0;
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
         
-        // distance , node
-        pq.push({0,S});
-        
-        while(!pq.empty()){
-            int d = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
-            for(auto v:adj[node]){
-                // for(auto x:v){
-                // cout << v[0] << " " << v[1] << endl;
-                    int cd = d + v[1];
-                    if(cd < ans[v[0]]){
-                        ans[v[0]] = cd;
-                        pq.push({cd,v[0]});
-                    }
-                // }
+        set<pair<int,int>> st;
+        st.insert({0,S});
+        while(st.size() >= 1){
+          auto it = st.begin();
+          int curNode = it -> second;
+          int dis = it -> first;
+            st.erase(it);
+            for(auto v:adj[curNode]){
+                int curDis = dis + v[1];
+                if(ans[v[0]] == INT_MAX){
+                    ans[v[0]] = curDis;
+                    st.insert({curDis, v[0]});
+                }
+                else if(curDis < ans[v[0]]){
+                    st.erase({ans[v[0]], v[0]});
+                    ans[v[0]] = curDis;
+                    st.insert({curDis,v[0]});
+                }
             }
         }
         return ans;
+        
+        
+        // // priority queue store distance of node from source in sorted order
+        // priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        
+        // // distance , node
+        // pq.push({0,S});
+        
+        // while(!pq.empty()){
+        //     int d = pq.top().first;
+        //     int node = pq.top().second;
+        //     pq.pop();
+        //     for(auto v:adj[node]){
+        //         int cd = d + v[1];
+        //         if(cd < ans[v[0]]){
+        //             ans[v[0]] = cd;
+        //             pq.push({cd,v[0]});
+        //         }
+        //     }
+        // }
+        // return ans;
     }
 };
 
